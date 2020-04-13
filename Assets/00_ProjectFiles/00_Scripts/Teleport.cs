dd87259c2b1c8bf22299c7d3d9a25a9d0809e658
npyalex <43121843+npyalex@users.noreply.github.com>
2020-04-12 21:33:31 -0400

    2020-04-12.2
    Updates to animation + mask on model

---
+++ Assets/00_ProjectFiles/00_Scripts/Teleport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject arriveHere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Transform other = collider.gameObject.transform.root;
+        Instantiate(other, new Vector3(arriveHere.transform.position.x, arriveHere.transform.position.y, arriveHere.transform.position.z), Quaternion.identity);
+        Destroy(other.gameObject);
+    }
+}
